a6f8655018581bb31d16c631d0e8d48d716441e0
Diki Saputra <diki@CNCRD.LAN>
2025-12-08 16:21:48 +0700

    update airchartei name to brunei

--- resources/views/pages/master/edithospital.blade.php
+++ resources/views/pages/master/edithospital.blade.php
@@ -913,7 +913,7 @@
           <div class="card card-outline card-info">
             <div class="card-header">
               <h3 class="card-title">
-                Edit Medical Support Website
+                Edit Emergency Medical Support
               </h3>
             </div>
             <!-- /.card-header -->
